d7155c082dacb77f6f0631f166c050c9ce8d1284
tdoria <doria.tiago@gmail.com>
2014-12-15 20:43:06 -0500

    Small adjustments

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 		<a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/github-wrap.png" alt="Github" class="social-icon"></a>
 	</div>
            
-  	<p class="footer-txt">Theme by <a href="http://twitter.com/tdoria">tdoria</a>. Some rights reserved <?php echo date('Y'); ?>.</p> 
+  	<p class="footer-txt">Day Street Theme by <a href="http://twitter.com/tdoria">tdoria</a>. Some rights reserved <?php echo date('Y'); ?>.</p> 
 	<p>Powered by <a href="https://wordpress.org/">WordPress</a></p>
 
 </footer>
@@ -15,5 +15,3 @@
  
   </body>
 </html>
-
-
